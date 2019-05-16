e816f0e6982f4b0e8effc5487ef8e64f1bdc7746
Rina Kawamura <rina.kawamura@yale.edu>
2019-05-16 17:10:45 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,8 @@ def my_collect(array)
   i = 0
   new_array = []
   while i < array.length
-    new_array << yield array[i]
+    elem = yield array[i]
+    new_array << elem
     i = i + 1
   end
   new_array
